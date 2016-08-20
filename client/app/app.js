2c8b79bf4fa9b4f6659f8083b7717a4f5c24c918
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2016-08-20 03:22:24 +0200

    Setup SW cache

--- client/app/app.js
+++ client/app/app.js
@@ -2,5 +2,17 @@ export let __hotReload = true;
 
 import R from 'ramda';
 import log from 'app/services/log';
+import offlineService from 'app/services/offline';
+
+self.STEAMDATING_APP = {
+  init: appInit
+};
 
 log(R);
+
+function appInit() {
+  log('STEAMDATING APP init');
+  if(!self.STEAMDATING_CONFIG.debug) {
+    offlineService.registerWorker();
+  }
+}
---
+++ client/app/services/offline.js
@@ -0,0 +1,69 @@
+export let __hotReload = true;
+
+import R from 'ramda';
+
+const offlineService = {
+  unregisterWorker: offlineUnregisterWorker,
+  registerWorker: offlineRegisterWorker
+};
+
+export default offlineService;
+
+function offlineUnregisterWorker() {
+  R.threadP()(
+    () => navigator.serviceWorker
+      .getRegistrations(),
+    (registrations) => {
+      for(let registration of registrations) {
+        console.info('Found worker', registration);
+        registration.unregister();
+      }
+    }
+  );
+}
+
+function offlineRegisterWorker() {
+  if(!('serviceWorker' in navigator)) {
+    console.warn('Service worker is not available');
+    return;
+  }
+
+  navigator.serviceWorker
+    .register('/service-worker.js', { scope: '/' })
+    .then(onRegistrationSuccess)
+    .catch((error) => {
+      console.log('Registration failed with ' + error);
+    });
+
+  function onRegistrationSuccess(reg) {
+    let installing;
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+    reg.addEventListener('updatefound', onUpdateFound);
+
+    function onUpdateFound() {
+      installing = reg.installing;
+      installing.addEventListener('statechange', onStateChange);
+    }
+
+    function onStateChange() {
+      switch(installing.state) {
+      case 'installed':
+        {
+          if(navigator.serviceWorker.controller) {
+            alert('New version available !');
+            self.location.reload(true);
+          }
+          else {
+            alert('Application installed !');
+          }
+          break;
+        }
+      case 'redundant':
+        {
+          console.warn('ServiceWorker became redundant');
+          break;
+        }
+      }
+    }
+  }
+}
