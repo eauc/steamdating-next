c91e52a738a8812f1eaefdfeb762b7411346ce6c
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2016-09-16 21:32:47 +0200

    Split auth0 from main bundle

--- client/app/components/auth/state.js
+++ client/app/components/auth/state.js
@@ -2,7 +2,7 @@ export let __hotReload = true;
 
 import R from 'app/helpers/ramda';
 import Joi from 'joi-browser';
-import Auth0Lock from 'auth0-lock';
+// import Auth0Lock from 'auth0-lock';
 import { dispatch,
          registerValidator } from 'app/services/state';
 import { registerInit } from 'app/services/init';
@@ -19,21 +19,35 @@ registerInit('auth', authInit);
 registerValidator('auth', scope, auth_schema);
 
 function authInit(state) {
-  lock = new Auth0Lock(
-    self.STEAMDATING_CONFIG.auth.client_id,
-    self.STEAMDATING_CONFIG.auth.domain,
-    { ui: { autoClose: true },
-      auth: { loginAfterSignup: true,
-              redirect: false,
-              params: { scope: 'openid email permissions' } } });
-  lock.on('authenticated', onLogin);
   return R.assocPath(scope, { token: null }, state);
 }
 
 export function authLogin() {
-  lock.show();
+  getLock().then((lock) => lock.show());
 }
 
 function onLogin(result) {
   dispatch(['auth-setToken', result.idToken]);
+}
+
+function getLock() {
+  if(!lock) {
+    return initLock();
+  }
+  return self.Promise.resolve(lock);
+}
+
+function initLock() {
+  return self.System.import('auth0-lock')
+    .then((Auth0Lock) => {
+      lock = new Auth0Lock(
+        self.STEAMDATING_CONFIG.auth.client_id,
+        self.STEAMDATING_CONFIG.auth.domain,
+        { ui: { autoClose: true },
+          auth: { loginAfterSignup: true,
+                  redirect: false,
+                  params: { scope: 'openid email permissions' } } });
+      lock.on('authenticated', onLogin);
+      return lock;
+    });
 }
--- client/app/helpers/react.js
+++ client/app/helpers/react.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactPerf from 'react-addons-perf';
-import {whyDidYouUpdate} from 'why-did-you-update';
 
 self.ReactPerf = ReactPerf;
 if(self.STEAMDATING_CONFIG.debug) {
-  whyDidYouUpdate(React);
+  System.import('why-did-you-update')
+    .then(({whyDidYouUpdate}) => {
+      whyDidYouUpdate(React);
+    });
 }
