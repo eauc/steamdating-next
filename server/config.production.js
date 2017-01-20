2ed9e49fc22d4c6e6ac33ed9532a4a4cde7d1780
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2017-01-20 23:13:58 +0100

    Auth0: update lock lib & change app client id

--- server/config.production.js
+++ server/config.production.js
@@ -3,7 +3,7 @@ module.exports = {
     tournaments: 'https://steamdating-data.herokuapp.com'
   },
   auth: {
-    client_id: 'CKGG9bWf1UJvwTrU0Ya8V8tUCN7vK27C',
+    client_id: 'vBhy4C4dLSkuoTbmldLYNxULGTyz6swK',
     domain: 'eauc.eu.auth0.com'
   }
 };
